5e1d209d3f6049faf7ded5bb94d50b8c9681e9a3
Yuliia Taratenko <86376480+YuliiaTa@users.noreply.github.com>
2025-05-12 02:27:17 +0300

    add events without api

--- src/main/java/com/yuliia/app/calendar/repository/EventRepository.java
+++ src/main/java/com/yuliia/app/calendar/repository/EventRepository.java
@@ -1,12 +1,13 @@
 package com.yuliia.app.calendar.repository;
 
+import com.yuliia.app.calendar.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.yuliia.app.calendar.model.Event;
-
-@Repository
-public interface EventRepository extends JpaRepository<Event, UUID> { }
-
+public interface EventRepository extends JpaRepository<Event, UUID> {
+    List<Event> findByDate(String date);
+    List<Event> findByOwnerId(UUID ownerId);
+    List<Event> findByOwnerIdAndDate(UUID ownerId, String date);
+}
